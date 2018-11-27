0fbe1d9d44ca370d972dfacf3aabd53d7622ab9f
aryabagaskara <aryagamis@gmail.com>
2018-11-27 13:12:27 +0700

    fixing numbers

--- src/main/java/com/tabeldata/bootcamp/App.java
+++ src/main/java/com/tabeldata/bootcamp/App.java
@@ -2,6 +2,7 @@ package com.tabeldata.bootcamp;
 
 import com.tabeldata.bootcamp.Model.Nasabah;
 import com.tabeldata.bootcamp.Model.Tabungan;
+
 import java.math.BigDecimal;
 import java.sql.*;
 import java.util.ArrayList;
@@ -10,20 +11,18 @@ import java.util.List;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
+public class App {
     static String url = "jdbc:postgresql://127.0.0.1:5432/saya";
     static String username = "saya";
     static String password = "saya";
     static Date sqlDate = new Date(Calendar.getInstance().getTimeInMillis());
-    public static void main( String[] args )
-    {
+
+    public static void main(String[] args) {
         Connection conn = null;
 
         try {
-            conn = DriverManager.getConnection(url,username,password);
+            conn = DriverManager.getConnection(url, username, password);
             conn.setAutoCommit(false);
             List<Nasabah> nasabah = new ArrayList<Nasabah>();
             String selectQuery = null;
@@ -35,29 +34,29 @@ public class App
             PreparedStatement statement = conn.prepareStatement(query);
 
             //inster data nasabah
-            statement.setString(1,"Johanes");
-            statement.setString(2,"Jl. Godong");
+            statement.setString(1, "Johanes");
+            statement.setString(2, "Jl. Godong");
             statement.executeUpdate();
 
 
             //insert data tabungan nasabah
             query = "INSERT INTO mst_tabungan (nasabah_id, saldo, tabungan_name) values (?,?,?)";
             statement = conn.prepareStatement(query);
-            statement.setInt(1,3);
+            statement.setInt(1, 3);
             statement.setBigDecimal(2, BigDecimal.valueOf(0));
-            statement.setString(3,"Tabungan Masa Depan");
+            statement.setString(3, "Tabungan Masa Depan");
             statement.executeUpdate();
             System.out.println("sukses memasukkan data tabungan baru");
 
             //select data tabungan dan insert transaksi
             Integer jenisTransaksi = 0;
             selectQuery = "SELECT saldo FROM mst_tabungan where tabungan_id=?";
-            switch(jenisTransaksi){
-                case 0 :
+            switch (jenisTransaksi) {
+                case 0:
                     selectStatement = conn.prepareStatement(selectQuery);
-                    selectStatement.setInt(1,1);
+                    selectStatement.setInt(1, 1);
                     resultSet = selectStatement.executeQuery();
-                    if (resultSet.next()){
+                    if (resultSet.next()) {
                         BigDecimal adder = null;
                         Tabungan tabungan = new Tabungan();
                         tabungan.setSaldo(resultSet.getBigDecimal(1));
@@ -66,18 +65,18 @@ public class App
                         statement = conn.prepareStatement(query);
                         statement.setInt(1, 1);
                         statement.setBigDecimal(2, BigDecimal.valueOf(200000));
-                        statement.setDate(3,sqlDate);
-                        tabungan.setSaldo(tabungan.getSaldo().add(BigDecimal.valueOf(300000)));
-                        adder= tabungan.getSaldo();
+                        statement.setDate(3, sqlDate);
+                        tabungan.setSaldo(tabungan.getSaldo().add(BigDecimal.valueOf(200000)));
+                        adder = tabungan.getSaldo();
                         statement.executeUpdate();
                         statement = conn.prepareStatement(selectQuery);
-                        statement.setBigDecimal(1,adder);
+                        statement.setBigDecimal(1, adder);
                         statement.executeUpdate();
                     }
                     break;
-                case 1 :
+                case 1:
                     selectStatement = conn.prepareStatement(selectQuery);
-                    selectStatement.setInt(1,1);
+                    selectStatement.setInt(1, 1);
                     resultSet = selectStatement.executeQuery();
                     if (resultSet.next()) {
                         BigDecimal adder = null;
@@ -87,18 +86,18 @@ public class App
                         selectQuery = "UPDATE mst_tabungan SET saldo=? WHERE tabungan_id=1";
                         statement = conn.prepareStatement(query);
                         statement.setInt(1, 1);
-                        statement.setBigDecimal(2, BigDecimal.valueOf(200000));
-                        tabungan.setSaldo(tabungan.getSaldo().subtract(BigDecimal.valueOf(300000)));
+                        statement.setBigDecimal(2, BigDecimal.valueOf(100000));
+                        tabungan.setSaldo(tabungan.getSaldo().subtract(BigDecimal.valueOf(100000)));
                         adder = tabungan.getSaldo();
                         statement.executeUpdate();
                         statement = conn.prepareStatement(selectQuery);
                         statement.setBigDecimal(1, adder);
                         statement.executeUpdate();
                     }
-                        break;
-                case 2 :
+                    break;
+                case 2:
                     selectStatement = conn.prepareStatement(selectQuery);
-                    selectStatement.setInt(1,1);
+                    selectStatement.setInt(1, 1);
                     resultSet = selectStatement.executeQuery();
                     if (resultSet.next()) {
                         BigDecimal adder = null;
@@ -109,7 +108,7 @@ public class App
                         statement = conn.prepareStatement(query);
                         statement.setInt(1, 1);
                         statement.setBigDecimal(2, BigDecimal.valueOf(2500));
-                        tabungan.setSaldo(tabungan.getSaldo().add(BigDecimal.valueOf(300000)));
+                        tabungan.setSaldo(tabungan.getSaldo().add(BigDecimal.valueOf(2500)));
                         adder = tabungan.getSaldo();
                         statement.executeUpdate();
                         statement = conn.prepareStatement(selectQuery);
@@ -117,7 +116,7 @@ public class App
                         statement.executeUpdate();
                     }
                     break;
-                default :
+                default:
                     System.out.println("jenis transaksi tidak ada");
 
 
@@ -132,7 +131,7 @@ public class App
         } catch (SQLException e) {
             System.out.println("Tidak bisa konek ke database");
             e.printStackTrace();
-            if (conn != null){
+            if (conn != null) {
                 try {
                     conn.rollback();
                 } catch (SQLException e1) {
